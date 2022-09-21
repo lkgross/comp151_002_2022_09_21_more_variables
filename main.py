f3a3a2604a2c03a40d966ed8eca45060fc37ebe5
L1Gross <profgross@gmail.com>
2022-09-21 15:13:45 -0400

    First commit

--- main.py
+++ main.py
@@ -1,16 +1,49 @@
-# This is a sample Python script.
+import math
+import random
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+string_value = "h"
+print(string_value)
+
+string_value = 'h'
+# This is the empty string:
+string_value = ""
+print(string_value)
+
+class_credits = 3
+class_grade = 4.0
+print(f"The number of credits for the class"
+      f" is {class_credits}.")
+print(class_grade)
+# Try using formatted strings (f-strings).
+print(f"The quality points are:\n\t{class_credits*class_grade}.")
+# There are escape characters "\n" and "\t" for newline character and tab.
+# You can call a method on a string (upper, lower, title, rstrip, and lstrip).
+msg = "     hEllo wOrld"
+print(msg)
+# Calling a function like print doesn't take dot notation.
+print()
+# Call the lower method on msg using dot notation.
+# Calling a method takes dot notation.
+print(msg.lower())
+# Here the returned value is "HELLO WORLD" is not printed.
+msg.upper()
+# Here the returned value is "HELLO WORLD" is printed.
+print(msg.upper())
+
+print(msg.title())
+print(msg)
+
+print(msg.lstrip())
+
+# Import and use the libraries math and random.
+# Look inside the math library to call the sqrt function!
+print(math.sqrt(9))
+
+# Look inside the random library to call the randrange function and
+# generate one of two random numbers, 0 or 1.
+print(random.randrange(2))
+print(random.randrange(2))
+print(random.randrange(2))
+print(random.randrange(2))
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
